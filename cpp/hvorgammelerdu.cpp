36b71f57146845946c13518e47965e8d4bb3e544
ev1stensberg <evenstensberg@gmail.com>
2019-02-01 14:10:56 +0100

    chore: add old projects

---
+++ cpp/hvorgammelerdu.cpp
@@ -0,0 +1,16 @@
+ #include <iostream.h>
+main()
+  {
+   int Alder;
+   cout << "Tast inn alderen din, i hele aar: ";
+   cin >> Alder;
+   
+   if (Alder <65)
+    cout << "Du er i yrkesaktiv alder,  ";  
+  else
+   cout << "Du maa vaere pensjonist     ";
+   
+   system("pause");
+   return 0;
+   
+  }
